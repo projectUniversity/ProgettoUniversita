a5b6e15746d4425aacd125a23c55179f42926fe5
collye <coly93@gmail.com>
2014-06-13 21:19:42 +0200

    Aggiunta del javadoc iniziale

--- MartaBalduzzi_SoniaBarbieri_ColetteSpelta_ProgettoCorso/src/InputSalvataggioEMenu/Menu.java
+++ MartaBalduzzi_SoniaBarbieri_ColetteSpelta_ProgettoCorso/src/InputSalvataggioEMenu/Menu.java
@@ -1,5 +1,11 @@
 package InputSalvataggioEMenu;
 
+/**
+ * 
+ * @author Balduzzi Marta, Barbieri Sonia e Spelta Colette
+ * Questa classe è stata curata da: Spelta Colette
+ *
+ */
 public class Menu {
 
 }
