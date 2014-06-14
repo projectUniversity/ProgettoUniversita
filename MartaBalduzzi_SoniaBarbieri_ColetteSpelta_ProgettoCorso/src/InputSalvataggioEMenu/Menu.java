4d23914671af524e4fc188f95de3576212f6ba22
collye <coly93@gmail.com>
2014-06-14 14:52:54 +0200

    Ho aggiunto il pacchetto con le classi dei prof e ho fatto la bozza del mio menu e del main.

--- MartaBalduzzi_SoniaBarbieri_ColetteSpelta_ProgettoCorso/src/InputSalvataggioEMenu/Menu.java
+++ MartaBalduzzi_SoniaBarbieri_ColetteSpelta_ProgettoCorso/src/InputSalvataggioEMenu/Menu.java
@@ -1,11 +1,46 @@
 package InputSalvataggioEMenu;
+import it.unibs.fp.mylib.*;
 
 /**
  * 
  * @author Balduzzi Marta, Barbieri Sonia e Spelta Colette
- * Questa classe e' a cura di Spelta Colette
+ * La classe e' stata cura da Spelta Colette
  *
  */
 public class Menu {
-  
+	//COSTANTI
+		public static final String CORNICE="*******************************************************************";
+		public static final String MSG_SCELTA="Digitare il numero dell'opzione desiderata --> ";
+		public static final String MSG_ESCI="0- Salva e termina";
+		
+		//ATTRIBUTI
+		private static String[] scelta;
+		
+		//COSTRUTTORE
+		/**
+		 * Costruttore Menu
+		 * Questo costruttore riceve in ingresso il vettore contenente tutte le scelte che l'utente può fare
+		 * @param _scelta
+		 */
+		public Menu(String [] _scelta){
+			scelta=_scelta;
+		}
+		
+		//METODI
+		/**
+		 * Metodo seleziona
+		 * Questo metodo stampa a video tutte le scelte possibili e legge l'intero inserito dall'utente
+		 * @return il numero intero che identifica la scelta dell'utente
+		 */
+		public int seleziona(){
+			System.out.println();
+			for(int i=0;i<scelta.length;i++){
+				System.out.println((i+1)+")"+scelta[i]);
+			}
+			System.out.println();
+			System.out.println(MSG_ESCI);
+			System.out.println(CORNICE);
+			return InputDati.leggiIntero(MSG_SCELTA, 0, scelta.length);
+		}
+
 }
--- MartaBalduzzi_SoniaBarbieri_ColetteSpelta_ProgettoCorso/src/mainETest/MainProgetto.java
+++ MartaBalduzzi_SoniaBarbieri_ColetteSpelta_ProgettoCorso/src/mainETest/MainProgetto.java
@@ -1,8 +1,72 @@
 package mainETest;
 
+import InputSalvataggioEMenu.Menu;
+
+/**
+ * 
+ * @author Balduzzi Marta, Barbieri Sonia e Spelta Colette
+ * La classe e' stata curata da Spelta Colette
+ *
+ */
 public class MainProgetto {
 	
+	/**
+	 *COSTANTI 
+	 */
+	public static final String[] scelte={"Inserisci i dati dell'utente","Prenota un nuovo esame","Inserisci l'esito di un esame","Inserisci una nuova malattia","Visualizza una malattia a scelta","Visualizza la cartella sanitaria"};
+	public static final String BENVENUTO="Benvenuto nel programma della cartella sanitaria";
+	
+	/**
+	 * ATTRIBUTI
+	 */
+	private static boolean finito=false;
+	private static Menu myMenu=new Menu(scelte);
+	private static int scelta;
+	
+	/**
+	 * METODI
+	 */
 	public static void main(String[] args){
-		
+		System.out.println(BENVENUTO);
+		do{
+			scelta=myMenu.seleziona();
+			switch(scelta){
+			case 1:
+				System.out.println();
+				//Queste stringhe verranno tolte, mi servono solo per controllare il funzionamento
+				System.out.println("Hai selezionato l'opzione per inserire i dati dell'utente");
+			break;
+			case 2:
+				System.out.println();
+				//Queste stringhe verranno tolte, mi servono solo per controllare il funzionamento
+				System.out.println("Hai selezionato l'opzione per la prenotazione di un esame");
+			break;
+			case 3:
+				System.out.println();
+				//Queste stringhe verranno tolte, mi servono solo per controllare il funzionamento
+				System.out.println("Hai selezionato l'opzione per inserire l'esito di un esame");
+			break;
+			case 4:
+				System.out.println();
+				//Queste stringhe verranno tolte, mi servono solo per controllare il funzionamento
+				System.out.println("Hai selezionato l'opzione per inserire una malattia");
+			break;
+			case 5:
+				System.out.println();
+				//Queste stringhe verranno tolte, mi servono solo per controllare il funzionamento
+				System.out.println("Hai selezionato l'opzione per scegliere la malattia da visualizzare");
+			break;
+			case 6:
+				System.out.println();
+				//Queste stringhe verranno tolte, mi servono solo per controllare il funzionamento
+				System.out.println("Hai selezionato l'opzione per visualizzare la cartella sanitaria");
+			break;
+			default:
+				System.out.println();
+				System.out.println("Grazie per aver utilizzato il nostro programma!");
+				finito=true;
+			}
+		}
+		while(!finito);
 	}
 }
