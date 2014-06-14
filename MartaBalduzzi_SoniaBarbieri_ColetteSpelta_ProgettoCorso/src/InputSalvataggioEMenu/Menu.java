e4b115aa88200df4ac1401e97570873699961597
sons94 <sony.-94-@hotmail.it>
2014-06-14 09:11:04 +0200

    cambiato il carattere nel javadoc perche' non si leggeva

--- MartaBalduzzi_SoniaBarbieri_ColetteSpelta_ProgettoCorso/src/InputSalvataggioEMenu/Menu.java
+++ MartaBalduzzi_SoniaBarbieri_ColetteSpelta_ProgettoCorso/src/InputSalvataggioEMenu/Menu.java
@@ -3,7 +3,7 @@ package InputSalvataggioEMenu;
 /**
  * 
  * @author Balduzzi Marta, Barbieri Sonia e Spelta Colette
- * Questa classe � a cura di Spelta Colette
+ * Questa classe e' a cura di Spelta Colette
  *
  */
 public class Menu {
