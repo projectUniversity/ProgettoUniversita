efebb82cbd1737a7a77d3cdeabb5af112ed390ae
sons94 <sony.-94-@hotmail.it>
2014-06-14 20:41:46 +0200

    Manca ancora il metodo per il controllo del codice fiscale e del gruppo sanguigno che provvedero' a sistemare domani

--- MartaBalduzzi_SoniaBarbieri_ColetteSpelta_ProgettoCorso/src/paziente_esami/Paziente.java
+++ MartaBalduzzi_SoniaBarbieri_ColetteSpelta_ProgettoCorso/src/paziente_esami/Paziente.java
@@ -2,6 +2,7 @@ package paziente_esami;
 
 import java.util.ArrayList;
 import java.util.GregorianCalendar;
+import it.unibs.fp.mylib.*;
 
 /**
  * Classe che rappresenta il paziente con i relativi dati anagrafici
@@ -19,7 +20,7 @@ public class Paziente
 	 */
 	public final static String FATTORE_POSITIVO = "POSITIVO";
 	public final static String FATTORE_NEGATIVO = "NEGATIVO";
-	public final static int GRUPPO_0 = 0;
+	public final static String GRUPPO_0 = "0";
 	public final static String GRUPPO_A = "A";
 	public final static String GRUPPO_B = "B";
 	public final static String GRUPPO_AB = "AB";
@@ -27,65 +28,68 @@ public class Paziente
 	public final static int NUM_TEL = 10;
 	public final static int CF_VUOTO = 0;
 	public final static int LUNGH_MAX_CF = 16;
-	public final static int LARGHEZZA_RIGA = 20;
 	
 	/**
 	 * ATTRIBUTI
 	 */
 	private String nome;
 	private String cognome;
-	private char genere;
+	private char sesso;
 	private double peso;
 	private double altezza;
-	GregorianCalendar dataNascita;
-	ArrayList <String> codiceFiscale;
+	private GregorianCalendar dataNascita;
+	private String [] codiceFiscale;
+	private String fattoreRh;
+	private String gruppoSanguigno;
 	private String viaCasa;
 	private String comuneCasa;
 	private String provinciaCasa;
 	private String comuneNascita;
 	private String provinciaNascita;
-	int [] capCasa;
-	int [] numTelefono;
-	ArrayList <Integer> telefoni;
+	private int [] capCasa;
+	private ArrayList <Integer> telefoni;
 	
 	/**
 	 * COSTRUTTORE
 	 * @param _nome il nome del paziente
 	 * @param _cognome il cognome del paziente
-	 * @param _genere genere del paziente (M/F)
-	 * @param _peso il peso del paziente (ex. 72.5)
-	 * @param _altezza l'altezza del paziente (ex. 1.35)
+	 * @param _sesso il sesso del paziente (M/F)
+	 * @param _peso il peso del paziente (ex. 72.5 kg)
+	 * @param _altezza l'altezza del paziente (ex. 1.5 m)
+	 * @param _fattoreRh il fattore Rh del sangue (positivo/negativo)
+	 * @param _gruppoSanguigno il gruppo sanguigno (0,A,B,AB)
 	 * @param _viaCasa la via dell'abitazione
-	 * @param _comuneCasa il comune dove abita
-	 * @param _provinciaCasa la provincia del comune dove abita
+	 * @param _comuneCasa il comune di dove abita
+	 * @param _provinciaCasa la provincia del comune del domicilio
 	 * @param _comuneNascita il comune dove e' nato
-	 * @param _provinciaNascita òa proovincia del comune di nascita
+	 * @param _provinciaNascita la provincia dove e' nato
 	 */
-	public Paziente(String _nome, String _cognome, char _genere, double _peso, double _altezza, String _viaCasa, String _comuneCasa, String _provinciaCasa, String _comuneNascita, String _provinciaNascita)
+	public Paziente(String _nome, String _cognome, char _sesso, double _peso, double _altezza, String _fattoreRh, String _gruppoSanguigno, String _viaCasa, String _comuneCasa, String _provinciaCasa, String _comuneNascita, String _provinciaNascita)
 	{
 		nome = _nome;
 		cognome = _cognome;
-		genere = _genere;
+		sesso = _sesso;
 		peso = _peso;
 		altezza = _altezza;
 		dataNascita = new GregorianCalendar ();
-		codiceFiscale = new ArrayList <String> ();
+		codiceFiscale = new String [LUNGH_MAX_CF];
+		fattoreRh = _fattoreRh;
+		gruppoSanguigno = _gruppoSanguigno;
 		viaCasa = _viaCasa;
 		comuneCasa = _comuneCasa;
 		provinciaCasa = _provinciaCasa;
 		comuneNascita = _comuneNascita;
 		provinciaNascita = _provinciaNascita;
 		capCasa = new int [NUM_CAP];
-		numTelefono = new int [NUM_TEL];
 		telefoni = new ArrayList <Integer> ();		
 	}
 	
 	//???????????????????????????????????????????????????????????????????????????
-	public boolean controlloCodiceFiscale(String msg)
+	public boolean controlloCf(String msg)
 	{
 		boolean giusto = false;
 		
-		if(codiceFiscale.size() == CF_VUOTO || codiceFiscale.size() != LUNGH_MAX_CF)
+		if(codiceFiscale.length != LUNGH_MAX_CF)
 		{
 			giusto = false;
 			System.out.println(msg); // Deve stampare un messaggio di errore!
@@ -95,22 +99,71 @@ public class Paziente
 		{
 			for(int i =0; i<=5; i++)
 			{
-				if(codiceFiscale.get(i));
+				if(codiceFiscale[i]);
 				
 			}
 		}
+		
+		return giusto;
 	}
 	
 	/**
-	 * METODO per la visualizzazione sintetica dei dati del paziente
+	 * METODO per la visualizzazione sintetica dei dati del paziente (nome e cognome)
 	 * @return il nome e il cognome del paziente
 	 */
 	public String toStringSintetico()
 	{
 		StringBuffer str = new StringBuffer();
+		str.append(BelleStringhe.incornicia("Dati del paziente"));
+		str.append("Nome: " + nome);
+		str.append("Cognome: " + cognome);
+		return str.toString();
+	}
+	
+	/**
+	 * METODO per la visualizzazione completa dei dati anagrafici del paziente
+	 * @return tutti i dati anagrafici del paziente
+	 */
+	public String toStringCompleto()
+	{
+		StringBuffer str = new StringBuffer();
+		str.append(BelleStringhe.incornicia("Dati anagrafici completi"));
+		str.append("Nome: " + nome);
+		str.append("Cognome: " + cognome);
+		str.append("Sesso: " + sesso + " - " + "Peso (kg) : " + peso + " - " + "Altezza (m): " + altezza);
+		str.append("Nato il: " + dataNascita + " a " + comuneNascita + " in provincia di " + provinciaNascita);
+		str.append("Abita in via " + viaCasa + " - " + comuneCasa + "In provincia di: " + provinciaCasa + " CAP: " + capCasa);
 		
-		str.append(BelleStringhe.centrata("Dati del paziente" + "\n" + "Nome: " + nome + "\n" + "Cognome: " + cognome);
+		for(int i=0; i<codiceFiscale.length; i++)
+		{
+			str.append("CF: " + codiceFiscale[i].toUpperCase());
+		}
+		
+		str.append("Gruppo sanguigno: " + gruppoSanguigno + fattoreRh);
+		
+		for(int i=0; i<telefoni.size(); i++)
+		{
+			str.append("Telefono/i: ");
+			int telefono = telefoni.get(i);
+			str.append(telefono);
+		}
 		
 		return str.toString();
+	}
+	
+	/**
+	 * METODO per aggiungere un numero di telefono
+	 * @param nuovoTel il numero di telefono che si vuole aggiungere
+	 */
+	public void aggiungiTel(int nuovoTel)
+	{
+		telefoni.add(nuovoTel);
+	}
+	
+	public boolean controlloGruppo()
+	{
+		boolean giusto = false;
+		
+		if()
 	}
 }
