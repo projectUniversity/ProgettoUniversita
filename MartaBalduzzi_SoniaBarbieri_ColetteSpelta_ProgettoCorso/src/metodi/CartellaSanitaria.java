4865f2f2da0d7b77046aa1795e0ca123869eafc3
collye <coly93@gmail.com>
2014-06-15 15:54:55 +0200

    Mi da l'errore con il toStringCompleto() di Paziente. Ho dovuto mettere tutti gli attributi di Paziente di tipo static perché se no dovevo ricreare l'istanza e non era semplice...

--- MartaBalduzzi_SoniaBarbieri_ColetteSpelta_ProgettoCorso/src/metodi/CartellaSanitaria.java
+++ MartaBalduzzi_SoniaBarbieri_ColetteSpelta_ProgettoCorso/src/metodi/CartellaSanitaria.java
@@ -1,5 +1,23 @@
 package metodi;
 
+import paziente_esami.Paziente;
+
 public class CartellaSanitaria {
 
+	/**
+	 * 
+	 * @author Balduzzi Marta, Barbieri Sonia e Spelta Colette
+	 * La classe e' stata curata da Spelta Colette
+	 *
+	 */
+	
+	//ATTRIBUTI
+	
+	
+	//METODI
+	public String toString(){
+		StringBuffer frase=new StringBuffer();
+		frase.append(String.format(Paziente.toStringCompleto()));
+		return frase.toString();
+	}
 }
