88ea9c2b8e352f3260bac7c8cd8bdd3b548dd31b
Calimarta94 <calimarta94@hotmail.it>
2014-06-16 12:03:49 +0200

    malattia_salvataggio

---
+++ MartaBalduzzi_SoniaBarbieri_ColetteSpelta_ProgettoCorso/src/malattia_salvataggio/LeggiDaFile.java
@@ -0,0 +1,16 @@
+package malattia_salvataggio;
+
+import java.io.*;
+
+/**
+ * Classe che permette di leggere i dati salvati su un file
+ * 
+ * @author Marta Balduzzi, Sonia Barbieri, Colette Spelta
+ * 
+ * Classe realizzata da Marta Balduzzi
+ */
+
+public class LeggiDaFile 
+{
+	
+}
---
+++ MartaBalduzzi_SoniaBarbieri_ColetteSpelta_ProgettoCorso/src/malattia_salvataggio/SalvaSuFile.java
@@ -0,0 +1,19 @@
+package malattia_salvataggio;
+
+import java.io.*;
+
+/**
+ * Classe usata per effettuare il salvataggio su un file
+ * 
+ * @author Marta Balduzzi, Sonia Barbieri, Colette Spelta
+ *
+ * Classe realizzata da Marta Balduzzi
+ */
+
+public class SalvaSuFile 
+{
+	public static void main (String [] args)
+	{
+		//Creare gli oggetti da salvare... e qui come faccio? XD
+	}
+}
