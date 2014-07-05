5cb1a45d83cead4042c7f5be13230f99cd3c1558
Calimarta94 <calimarta94@hotmail.it>
2014-07-05 21:40:29 +0200

    Javadoc ufficiale

--- MartaBalduzzi_SoniaBarbieri_ColetteSpelta_ProgettoCorso/src/malattia_salvataggio/Malattia.java
+++ MartaBalduzzi_SoniaBarbieri_ColetteSpelta_ProgettoCorso/src/malattia_salvataggio/Malattia.java
@@ -68,55 +68,8 @@ public class Malattia implements Serializable
 		diagnosi = _diagnosi;
 		esame = _esame;
 		terapia = _terapia;
-		/**sintomiMalattia = _sintomiMalattia;
-		diagnosiMalattia = _diagnosiMalattia;
-		esamiAssociati = _esamiAssociati;
-		terapiaAssociata = _terapiaAssociata;**/
 	}
-	
-	/**
-	 * Metodi per aggiungere sintomi, diagnosi, esami associati o terapie
-	 */
-	/**public static void aggiungiSintomi (String nuovoSintomo)
-	{
-		sintomiMalattia.add(nuovoSintomo);
-	}
-	
-	public static void aggiungiDiagnosi (String nuovaDiagnosi)
-	{
-		diagnosiMalattia.add(nuovaDiagnosi);
-	}
-	
-	public static void aggiungiEsami (String nuovoEsame)
-	{
-		esamiAssociati.add(nuovoEsame);
-	}
-	
-	public static void aggiungiTerapia (String nuovaTerapia)
-	{
-		terapiaAssociata.add(nuovaTerapia);
-	}**/
-	
-	
-	/**
-	 * Metodo per inserire la data di inizio malattia
-	 */
-	/**public void inizioMalattia()
-	{
-		giornoInizio = dataInizioMalattia.get(GregorianCalendar.DATE);
-		meseInizio = dataInizioMalattia.get(GregorianCalendar.MONTH)+1;
-		annoInizio = dataInizioMalattia.get(GregorianCalendar.YEAR);
-	}**/
-	
-	/**
-	 * Metodo per inserire la data di fine malattia
-	 */
-	/**public void fineMalattia()
-	{
-		giornoFine = dataFineMalattia.get(GregorianCalendar.DATE);
-		meseFine = dataFineMalattia.get(GregorianCalendar.MONTH)+1;
-		annoFine = dataFineMalattia.get(GregorianCalendar.YEAR);
-	}**/
+		
 	
 	/**
 	 * Metodo per visualizzare un toString sintetico per descrivere la malattia
